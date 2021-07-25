da94078ac7ab69031ab790534228778e5ad375cc
niranjan <niranjan@LAPTOP-HVCTORMS>
2021-07-25 17:28:25 +0530

    Commited by arun

---
+++ ImmutableClass/src/com/vidvaan/immutabledemo/ImmutableStudent.java
@@ -0,0 +1,20 @@
+package com.vidvaan.immutabledemo;
+
+public final class ImmutableStudent {
+
+	private final int id;
+	private final String name;
+
+	public ImmutableStudent(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
